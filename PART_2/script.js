8efb98c0caa0b4b4658ae9380d208839edc89dfc
Bishal Pandey <bishalpandey.im@gmail.com>
2026-01-18 15:41:58 +0545

    Basic Nodejs foundation , Npm basics and Advanced features are covered

---
+++ PART_2/script.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+fs.writeFile("./PART_2/Create.txt", "Mero mailo commit hai", function (err) {
+  if (err) {
+    console.log(err);
+  } else console.log("Vayo");
+});
+
+fs.appendFile("Create.txt", "\nYo Kura chai append garam hai", function (err) {
+  if (err) {
+    console.log(err);
+  } else console.log("Vayo");
+});
+
+fs.rename("Create.txt", "Change.txt", (err) => {
+  if (err) {
+    console.log(err);
+  } else console.log("Aayena Error");
+});
+
+fs.copyFile("Change.txt", "Copy.txt", (err) => {
+  if (err) {
+    console.log(err);
+  } else console.log("Vayo Copy pani");
+});
+
+fs.unlink("Copy.txt", (err) => {
+  if (err) console.log("Error occurred");
+  else console.log("File deleted successfully");
+});
+
+fs.rmdir("./rmdir_FOL", (err) => {
+  if (err) console.log(err.message);
+  else console.log("Directory removed");
+});
+
+fs.rm("./try_DELETE", { recursive: true }, (err) => {
+  if (err) console.log(err.message);
+  else console.log("Directory removed");
+});
